e013c07c89f7ff38a657f150d598c631be589065
gaizm <gaizm@DESKTOP-HIK20SV>
2024-10-17 18:27:20 +0200

    hecho sin barro

--- Psp/src/ej2_2/MainHijo1erproceso.java
+++ Psp/src/ej2_2/MainHijo1erproceso.java
@@ -4,9 +4,15 @@ import java.io.File;
 
 public class MainHijo1erproceso {
 
-	public static void main(String[] args) {
-		File file = new File("fichero.txt");
-		ManejarFicheros mf = new ManejarFicheros("a", file);
-	}
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Debe proporcionar una vocal como argumento.");
+            return;
+        }
 
+        String vocal = args[0];
+        File file = new File("fichero.txt");
+        ManejarFicheros mf = new ManejarFicheros(vocal, file);
+        mf.readfile();
+    }
 }
--- Psp/src/ej2_2/MainPadre.java
+++ Psp/src/ej2_2/MainPadre.java
@@ -1,5 +1,42 @@
 package ej2_2;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainPadre {
 
+    public static void main(String[] args) {
+        // Lista de caracteres (vocales) que vamos a procesar
+        String[] vocales = {"a", "e", "i", "o", "u"};
+        
+        // Lista para almacenar los procesos
+        List<Process> procesos = new ArrayList<>();
+        
+        // Crear y lanzar los procesos hijos
+        for (String vocal : vocales) {
+            try {
+                // Crear un nuevo proceso con ProcessBuilder
+                ProcessBuilder pb = new ProcessBuilder("java", "ej2_2.MainHijo1erproceso", vocal);
+                Process proceso = pb.start();
+                
+                // Añadir el proceso a la lista para su gestión
+                procesos.add(proceso);
+                System.out.println("Lanzado proceso para vocal: " + vocal);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Esperar a que todos los procesos terminen
+        for (Process proceso : procesos) {
+            try {
+                proceso.waitFor();  // Espera a que termine el proceso
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        System.out.println("Todos los procesos han terminado.");
+    }
 }
